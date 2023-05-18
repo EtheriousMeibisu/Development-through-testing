a60f9c644f1f255f820a766b189fd6d5ad8fe83a
Yulya Lukicheva <104934302+DrowsyAlchemist@users.noreply.github.com>
2023-05-18 12:31:30 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,21 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+
+    @Test
+    public void addTest(){
+        final int numberContactsExpected = 1;
+
+        final int actual = PhoneBook.add();
+
+        Assertions.assertEquals(numberContactsExpected, actual);
+    }
+
+    @Test
+    public void addTestRel(){
+
+    }
+
 }
