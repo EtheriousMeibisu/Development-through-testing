01d3e6504b9563ad1fbfc1ef5d75fd22a1eb049a
Yulya Lukicheva <104934302+DrowsyAlchemist@users.noreply.github.com>
2023-05-18 12:41:41 +0300

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,8 +1,15 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
 
-    public static int add(){
-        return 0;
+    private static Map<String,String> phoneBook = new HashMap<>();
+
+    public static int add(String name, String  numberPhone){
+        phoneBook.put(name, numberPhone);
+        return phoneBook.size();
+
     }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -8,14 +8,8 @@ public class PhoneBookTest {
     public void addTest(){
         final int numberContactsExpected = 1;
 
-        final int actual = PhoneBook.add();
+        final int actual = PhoneBook.add("Юлия" , "8996240");
 
         Assertions.assertEquals(numberContactsExpected, actual);
     }
-
-    @Test
-    public void addTestRel(){
-
-    }
-
 }
